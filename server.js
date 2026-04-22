14dcce5d264aa2c6d2076c39c1604fc8ef74473e
YassineSBAI <164426658+YassineSBAI@users.noreply.github.com>
2026-04-22 10:51:27 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,26 +3,27 @@ const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
-// Tell the server to serve your HTML file
 app.use(express.static('public'));
 
-// This keeps track of the color so if a friend joins LATE, 
-// they still see the green background if someone already clicked it.
 let isGreen = false; 
 
 io.on('connection', (socket) => {
   console.log('A user connected');
 
-  // If the page is already green, tell the new user
   if (isGreen) {
     socket.emit('colorChange', 'green');
   }
 
-  // Listen for the "3afak" button click
+  // Turns it green for everyone
   socket.on('buttonClicked', () => {
     isGreen = true;
-    // 'io.emit' sends the message to EVERYONE who is connected
     io.emit('colorChange', 'green'); 
+  });
+
+  // NEW: Turns it black for everyone and resets the server memory
+  socket.on('resetClicked', () => {
+    isGreen = false;
+    io.emit('colorChange', 'black');
   });
 });
 
